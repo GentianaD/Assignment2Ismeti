9bf34675ed960f6f0f139fda547dfad6516d4235
ladishala <lavdishala@hotmail.com>
2013-10-04 21:13:55 +0200

    try

--- src/assignments/ismeti/GPSCallBack.java
+++
@@ -1,7 +0,0 @@
-package assignments.ismeti;
-import android.location.Location;
-public interface GPSCallBack {
-
-       public abstract void onGPSUpdate(Location location);
-	
-}
--- src/com/example/gpsexample/GPSCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.example.gpsexample;
-import android.location.Location;
-public interface GPSCallBack {
-
-       public abstract void onGPSUpdate(Location location);
-	
-}
